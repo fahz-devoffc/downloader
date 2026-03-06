b4205c70b1911571e0d31dc99b1c4a97c49bb5e9
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2026-03-06 15:16:27 +0700

    Update convert.js

--- api/convert.js
+++ api/convert.js
@@ -1,1 +1,45 @@
 
+import axios from "axios"
+
+export default async function handler(req,res){
+
+if(req.method !== "POST"){
+return res.status(405).json({error:"Method not allowed"})
+}
+
+const API_KEY = "1bf1ad83f3d9f099cf55533d102b38749e1eee2006c50cdd77aa2d575695654e"
+
+const {url,format}=req.body
+
+try{
+
+const response = await axios.get("https://back.asitha.top/api/ytapi",{
+params:{
+url:url,
+fo: format==="mp3" ? "1":"2",
+qu:"128"
+},
+headers:{
+Authorization:`Bearer ${API_KEY}`
+}
+})
+
+const data=response.data
+
+res.status(200).json({
+status:true,
+title:data.title,
+thumbnail:data.thumbnail,
+link:data.link
+})
+
+}catch(err){
+
+res.status(500).json({
+status:false
+})
+
+}
+
+}
+
